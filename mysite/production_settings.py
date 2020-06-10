b9cdf5a80cb38b564e0db8cf409f76efaf04047f
zooeytsai <s1301h030@gmail.com>
2020-06-11 01:23:45 +0800

    change homepage

--- mysite/production_settings.py
+++ mysite/production_settings.py
@@ -2,19 +2,6 @@ from .settings import *
 import dj_database_url
 
 
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.sqlite3',
-        'NAME': '/Users/zooeytsai/PycharmProjects/mysite/db.sqlite3',
-    },
-    'agile_db': {
-        'ENGINE':'django.db.backends.sqlite3',
-        'NAME': '/Users/zooeytsai/PycharmProjects/mysite/agile_tai.db',
-    },
-}
-
-DATABASE_ROUTERS = ['db_router.AgileRouter']
-
 STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
