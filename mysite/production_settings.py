ddc0bddd125eb13f27f85a91f2829c92927f0bde
zooeytsai <s1301h030@gmail.com>
2020-06-17 23:12:03 +0800

    change pd_settings

--- mysite/production_settings.py
+++ mysite/production_settings.py
@@ -1,16 +1,6 @@
 from .settings import *
 
 
-DATABASES = {
-    'agile_db': {
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'agile',
-        'USER':'zooeytsai',
-        'PASSWORD':'tyla910ai',
-        'HOST':'localhost',
-        'PORT':'5432',
-    },
-}
 STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
