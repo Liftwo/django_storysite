43b3c11534d77cc9c3d3f0809f8e755ae6fb47d8
zooeytsai <s1301h030@gmail.com>
2020-06-19 00:51:06 +0800

    change pd_settings

--- mysite/production_settings.py
+++ mysite/production_settings.py
@@ -5,4 +5,4 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
-DEBUG = False
+DEBUG = True
