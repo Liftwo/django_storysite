b70fd27b99b501a275b50fa2dbf2f78c815aeae0
zooeytsai <s1301h030@gmail.com>
2021-01-04 00:41:22 +0800

    coreheaders

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    'django-cors-headers',
     'classes',
     'agile',
 ]
@@ -160,4 +160,3 @@ CORS_ALLOW_ORIGIN = (
     'http://localhost:63342',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
