b0c67244fa428dd73f7d3afb0eb2fb3fe3fd1663
zooeytsai <s1301h030@gmail.com>
2021-01-07 00:47:13 +0800

    新增忘記密碼的form

--- agile/forms.py
+++ agile/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 
+
 class RegisterForm(forms.Form):
     username = forms.CharField(max_length=20, required=True)
     password = forms.CharField(max_length=20, required=True)
+
+
+class ForgetFrom(forms.Form):
+    username = forms.CharField(max_length=20, required=True)
+
+
+class ResetForm(forms.Form):
+    newpwd = forms.CharField(max_length=20, required=True, error_messages={'required':'密碼不能為空'})
+    renewpwd = forms.CharField(max_length=20, required=True, error_messages={'required':'密碼不能為空'})
