37f0f485369b630672110a7071b0b5dcfc4ecde2
zooeytsai <s1301h030@gmail.com>
2021-01-26 01:35:16 +0800

    allow_host

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '!df2%&mo!4=!&f%&3d(-qjz86g6sh9ei+om69xc39t-0r1pwvr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
